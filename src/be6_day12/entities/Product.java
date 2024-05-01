53d467c97007580928d760b5539d6b69a5d301ee
Thuy Nguyen <104875637+ThuyKat@users.noreply.github.com>
2024-05-01 13:44:08 +1000

    Update Product.java

--- src/be6_day12/entities/Product.java
+++ src/be6_day12/entities/Product.java
@@ -2,7 +2,7 @@ package be6_day12.entities;
 
 public class Product {
 
-//public int quantity; //THIS SHOULD BE MOVED INTO DTO- ITEMS IN CART
+
 public int productID;
 public String productName;
 public double price;
@@ -10,7 +10,6 @@ public Product(int productID,String productName, double price) {
 	
 	this.productID = productID;
 	this.productName = productName;
-//	this.quantity = quantity;
 	this.price = price;
 }
 }
